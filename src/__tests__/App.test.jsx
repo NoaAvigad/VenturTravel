372ab89d196499271f926f2ada8bfefeea100e77
Mauricio Mercado <mau@maumercado.com>
2019-06-22 22:17:36 -0700

    refactor(all): refactor/reorganized code and add linter precommit

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import NavigationTestUtils from "react-navigation/NavigationTestUtils";
 
 import App from "../App";
 
 describe("<App />", () => {
-  beforeEach(() => {
-    NavigationTestUtils.resetInternalState();
-  });
-
   it("renders without crashing", () => {
-    const rendered = renderer.create(<App />);
+    const rendered = renderer.create(<App />).toJSON();
     expect(rendered).toBeTruthy();
   });
 });
